9d16d826abb43a3bd59f10abbafebed6c23fa418
Joao-Pedro-Cabral <miranda.jp@usp.br>
2025-02-19 15:08:09 +0000

    Fixing error message

--- debug.h
+++ debug.h
@@ -3,13 +3,13 @@
 #define __DEBUG_H
 #ifdef DEBUG_H
 #define DEBUG(str, ...) printf(str, ##__VA_ARGS__)
-#define ERROR(fmt, ...)                                                        \
+#define ERROR(str, ...)                                                        \
   do {                                                                         \
-    printf("ERROR: " fmt "\n", ##__VA_ARGS__);                                 \
+    printf(str, ##__VA_ARGS__);                                 \
     exit(1);                                                                   \
   } while (0)
 #else
 #define DEBUG(str, ...) ((void)0)
-#define ERROR(fmt, ...) exit(1)
+#define ERROR(str, ...) exit(1)
 #endif // DEBUG_H
 #endif // __DEBUG_H
