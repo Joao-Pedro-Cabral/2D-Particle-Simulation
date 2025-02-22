ee775aab94426e535da42f6467f2e6f36fd9ed7b
Joao-Pedro-Cabral <miranda.jp@usp.br>
2025-02-22 11:25:17 +0000

    Formatting code

--- parsim.cpp
+++ parsim.cpp
@@ -15,10 +15,10 @@ void fill_cells(double size, long ncside, long long n_part,
 void debug_center(long ncside, const std::vector<cell_t> &cells) {
   for (long iy = 0; iy < ncside + 2; iy++) {
     for (long ix = 0; ix < ncside + 2; ix++) {
-      long i = iy*(ncside + 2) + ix;
-    DEBUG("Cell %ld x: %.3lf y: %.3lf m: %.3lf\n", i, cells[i].center.x,
-          cells[i].center.y, cells[i].center.m);
-  }
+      long i = iy * (ncside + 2) + ix;
+      DEBUG("Cell %ld x: %.3lf y: %.3lf m: %.3lf\n", i, cells[i].center.x,
+            cells[i].center.y, cells[i].center.m);
+    }
   }
 }
 
@@ -28,54 +28,58 @@ void compute_centers_of_mass(double side, long ncside,
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    double total_mass = 0.0;
-    double weighted_x = 0.0;
-    double weighted_y = 0.0;
+      double total_mass = 0.0;
+      double weighted_x = 0.0;
+      double weighted_y = 0.0;
 
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      total_mass += cells[i].par[j].m;
-      weighted_x += cells[i].par[j].m * cells[i].par[j].x;
-      weighted_y += cells[i].par[j].m * cells[i].par[j].y;
-    }
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        total_mass += cells[i].par[j].m;
+        weighted_x += cells[i].par[j].m * cells[i].par[j].x;
+        weighted_y += cells[i].par[j].m * cells[i].par[j].y;
+      }
 
-    cells[i].center.m = total_mass;
+      cells[i].center.m = total_mass;
 
-    if (total_mass > 0) {
-      cells[i].center.x = weighted_x / total_mass;
-      cells[i].center.y = weighted_y / total_mass;
-    } else {
-      DEBUG("Warning: Cell %ld has no particles. Setting default center of "
-            "mass to (-2side, -2side)\n",
-            i);
-      cells[i].center.x = -2*side;
-      cells[i].center.y = -2*side;
-    }
+      if (total_mass > 0) {
+        cells[i].center.x = weighted_x / total_mass;
+        cells[i].center.y = weighted_y / total_mass;
+      } else {
+        DEBUG("Warning: Cell %ld has no particles. Setting default center of "
+              "mass to (-2side, -2side)\n",
+              i);
+        cells[i].center.x = -2 * side;
+        cells[i].center.y = -2 * side;
+      }
     }
   }
-  for(long i = 0; i < ncside + 2; i += ncside + 1) {
-    for(long j = 0; j < ncside + 2; j++) {
-      long ind = i*(ncside + 2) + j;
+  for (long i = 0; i < ncside + 2; i += ncside + 1) {
+    for (long j = 0; j < ncside + 2; j++) {
+      long ind = i * (ncside + 2) + j;
       long i2 = (i == 0) ? ncside : 1;
-      if(j == 0 || j == ncside + 1) {
+      if (j == 0 || j == ncside + 1) {
         long j2 = (j == 0) ? ncside : 1;
-        long ind2  = i2*(ncside + 2) + j2;
-        cells[ind].center.x = (j == 0) ? cells[ind2].center.x - side : cells[ind2].center.x + side;
-        cells[ind].center.y = (i == 0) ? cells[ind2].center.y - side : cells[ind2].center.y + side;
+        long ind2 = i2 * (ncside + 2) + j2;
+        cells[ind].center.x = (j == 0) ? cells[ind2].center.x - side
+                                       : cells[ind2].center.x + side;
+        cells[ind].center.y = (i == 0) ? cells[ind2].center.y - side
+                                       : cells[ind2].center.y + side;
         cells[ind].center.m = cells[ind2].center.m;
       } else {
-        long ind2  = i2*(ncside + 2) + j;
-        cells[ind].center.x = cells[ind2].center.x; 
-        cells[ind].center.y = (i == 0) ? cells[ind2].center.y - side : cells[ind2].center.y + side;
+        long ind2 = i2 * (ncside + 2) + j;
+        cells[ind].center.x = cells[ind2].center.x;
+        cells[ind].center.y = (i == 0) ? cells[ind2].center.y - side
+                                       : cells[ind2].center.y + side;
         cells[ind].center.m = cells[ind2].center.m;
       }
     }
   }
-  for(long j = 0; j < ncside + 2; j += ncside + 1) {
-    for(long i = 1; i < ncside + 1; i++) {
-      long ind = i*(ncside + 2) + j;
+  for (long j = 0; j < ncside + 2; j += ncside + 1) {
+    for (long i = 1; i < ncside + 1; i++) {
+      long ind = i * (ncside + 2) + j;
       long j2 = (j == 0) ? ncside : 1;
-      long ind2 = i*(ncside + 2) + j2;
-      cells[ind].center.x = (j == 0) ? cells[ind2].center.x - side : cells[ind2].center.x + side;
+      long ind2 = i * (ncside + 2) + j2;
+      cells[ind].center.x =
+          (j == 0) ? cells[ind2].center.x - side : cells[ind2].center.x + side;
       cells[ind].center.y = cells[ind2].center.y;
       cells[ind].center.m = cells[ind2].center.m;
     }
@@ -83,7 +87,7 @@ void compute_centers_of_mass(double side, long ncside,
   debug_center(ncside, cells);
 }
 
-void debug_accelerations(const vec_t& force, long ind) {
+void debug_accelerations(const vec_t &force, long ind) {
   DEBUG("Force x: %.3lf, y: %.3lf, ind : %ld\n", force.x, force.y, ind);
 }
 
@@ -94,32 +98,34 @@ void compute_accelerations(long ncside, const std::vector<cell_t> &cells,
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      double resultant_x = 0.0;
-      double resultant_y = 0.0;
-      for (long long k = 0; k < cells[i].par.size(); k++) {
-        if (k == j)
-          continue;
-        vec_t force =
-            calc_gravitacional_force(cells[i].par[j], cells[i].par[k]);
-        resultant_x += force.x;
-        resultant_y += force.y;
-        if(cells[i].par[j].ind == 0) debug_accelerations(force, 0);
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        double resultant_x = 0.0;
+        double resultant_y = 0.0;
+        for (long long k = 0; k < cells[i].par.size(); k++) {
+          if (k == j)
+            continue;
+          vec_t force =
+              calc_gravitacional_force(cells[i].par[j], cells[i].par[k]);
+          resultant_x += force.x;
+          resultant_y += force.y;
+          if (cells[i].par[j].ind == 0)
+            debug_accelerations(force, 0);
+        }
+        for (long long k = 0; k < 9; k++) {
+          if (k == 4)
+            continue;
+          long ind = i + ((k % 3) - 1) + (k / 3 - 1) * (ncside + 2);
+          vec_t force =
+              calc_gravitacional_force(cells[i].par[j], cells[ind].center);
+          resultant_x += force.x;
+          resultant_y += force.y;
+          if (cells[i].par[j].ind == 0)
+            debug_accelerations(force, ind);
+        }
+        accs[l].x = resultant_x / cells[i].par[j].m;
+        accs[l].y = resultant_y / cells[i].par[j].m;
+        l++;
       }
-      for (long long k = 0; k < 9; k++) {
-        if (k == 4)
-          continue;
-        long ind = i + ((k % 3) - 1) + (k / 3 - 1) * (ncside + 2);
-        vec_t force =
-            calc_gravitacional_force(cells[i].par[j], cells[ind].center);
-        resultant_x += force.x;
-        resultant_y += force.y;
-        if(cells[i].par[j].ind == 0) debug_accelerations(force, ind);
-      }
-      accs[l].x = resultant_x / cells[i].par[j].m;
-      accs[l].y = resultant_y / cells[i].par[j].m;
-      l++;
-    }
     }
   }
 }
@@ -131,24 +137,24 @@ void compute_new_positions_and_velocities(double side, double size, long ncside,
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      update_position_and_velocity(side, cells[i].par[j], accs[l]);
-      l++;
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        update_position_and_velocity(side, cells[i].par[j], accs[l]);
+        l++;
+      }
     }
-  }
   }
 
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    for (long long j = cells[i].par.size() - 1; j > 0; j--) {
-      long ind = find_particle_cell(cells[i].par[j], size, ncside);
-      if (ind == i)
-        continue;
-      cells[ind].par.push_back(cells[i].par[j]);
-      remove_and_swap(cells, i, j);
+      for (long long j = cells[i].par.size() - 1; j > 0; j--) {
+        long ind = find_particle_cell(cells[i].par[j], size, ncside);
+        if (ind == i)
+          continue;
+        cells[ind].par.push_back(cells[i].par[j]);
+        remove_and_swap(cells, i, j);
+      }
     }
-  }
   }
 }
 
@@ -158,23 +164,23 @@ long long detect_collisions(long ncside, std::vector<cell_t> &cells,
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      collisions[j] = false;
-      for (long long k = 0; k < j; k++) {
-        double distance = calc_distance(cells[i].par[j], cells[i].par[k]);
-        if (distance > DELTAT * DELTAT)
-          continue;
-        if (collisions[i] == false)
-          n_collisions++;
-        collisions[j] = true;
-        collisions[i] = true;
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        collisions[j] = false;
+        for (long long k = 0; k < j; k++) {
+          double distance = calc_distance(cells[i].par[j], cells[i].par[k]);
+          if (distance > DELTAT * DELTAT)
+            continue;
+          if (collisions[i] == false)
+            n_collisions++;
+          collisions[j] = true;
+          collisions[i] = true;
+        }
       }
-    }
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      if (collisions[j] == false)
-        continue;
-      remove_and_swap(cells, i, j);
-    }
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        if (collisions[j] == false)
+          continue;
+        remove_and_swap(cells, i, j);
+      }
     }
   }
   return n_collisions;
@@ -184,10 +190,10 @@ particle_t find_particle_zero(long ncside, std::vector<cell_t> &cells) {
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = POS(ix, iy, ncside);
-    for (long long j = 0; j < cells[i].par.size(); j++) {
-      if (cells[i].par[j].ind == 0)
-        return cells[i].par[j];
-    }
+      for (long long j = 0; j < cells[i].par.size(); j++) {
+        if (cells[i].par[j].ind == 0)
+          return cells[i].par[j];
+      }
     }
   }
   ERROR("Particle zero not found\n");
