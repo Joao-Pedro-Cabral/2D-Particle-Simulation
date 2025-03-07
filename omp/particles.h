a7d6356a43580b0bfda66bad5e96d22f8fecc66b
Lisa Santarossa <santarossa.lisa@gmail.com>
2025-03-07 14:45:54 +0000

    adding omp pragmas to main_with_simd

--- omp/particles.h
+++ omp/particles.h
@@ -10,17 +10,25 @@ typedef struct {
   double y;
   double vx;
   double vy;
-  double ax;
-  double ay;
   double m;
-  long long ind;
-  bool collided;
 } particle_t;
 
-typedef struct {
+typedef struct cell_t {
   particle_t center;
-  std::vector<particle_t> par;
+  std::vector<double> x;
+  std::vector<double> y;
+  std::vector<double> vx;
+  std::vector<double> vy;
+  std::vector<double> ax;
+  std::vector<double> ay;
+  std::vector<double> m;
+  std::vector<long long> ind;
+  std::vector<bool> collided;
   omp_lock_t lock;
+
+  void push_back(const particle_t &par, long long i);
+  void push_back(const cell_t &cell, long long i);
+  void resize(long long size);
 } cell_t;
 
 typedef struct {
@@ -31,10 +39,14 @@ typedef struct {
 #define INDEX(x, y, ncside) ((y + 1) * (ncside + 2) + (x + 1))
 
 long find_particle_cell(const particle_t &par, double size, long ncside);
-void copy_particle(particle_t &par1, const particle_t &par2);
-void remove_and_swap(std::vector<cell_t> &cells, long i, long long j);
-double calc_squared_distance(const particle_t &par1, const particle_t &par2);
-void update_position_and_velocity(double side, particle_t &par);
-vec_t calc_gravitational_force(const particle_t &par1, const particle_t &par2);
+long find_particle_cell(const cell_t &cell, long long i, double size,
+                        long ncside);
+void copy_particle(particle_t &par1, const cell_t &cell, long long i);
+void copy_particle(cell_t &cell, long long i, long long j);
+double calc_squared_distance(const cell_t &cell, long long i, long long j);
+void update_position_and_velocity(double side, cell_t &cell, long long i);
+vec_t calc_gravitational_force(const cell_t &cell, long long i, long long j);
+vec_t calc_gravitational_force(const cell_t &cell, long long i,
+                               const particle_t &center);
 
 #endif // __PARTICLES_H
--- omp/simulation.cpp
+++ omp/simulation.cpp
@@ -5,13 +5,11 @@
 #include <vector>
 
 void fill_cells(double size, long ncside, long long n_part,
-                std::vector<particle_t> &par, std::vector<cell_t> &cells) {
+                const std::vector<particle_t> &par,
+                std::vector<cell_t> &cells) {
   for (long long i = 0; i < n_part; i++) {
-    par[i].ax = 0.0;
-    par[i].ay = 0.0;
-    par[i].ind = i;
-    par[i].collided = false;
-    cells[find_particle_cell(par[i], size, ncside)].par.push_back(par[i]);
+    cell_t &cell = cells[find_particle_cell(par[i], size, ncside)];
+    cell.push_back(par[i], i);
   }
 }
 
@@ -54,22 +52,23 @@ void compute_centers_of_mass(double side, long ncside,
       double total_mass = 0.0;
       double weighted_x = 0.0;
       double weighted_y = 0.0;
-      long long cell_size = cells[i].par.size();
+      cell_t &cell = cells[i];
+      long long cell_size = cell.x.size();
 
       for (long long j = 0; j < cell_size; j++) {
-        total_mass += cells[i].par[j].m;
-        weighted_x += cells[i].par[j].m * cells[i].par[j].x;
-        weighted_y += cells[i].par[j].m * cells[i].par[j].y;
+        total_mass += cell.m[j];
+        weighted_x += cell.m[j] * cell.x[j];
+        weighted_y += cell.m[j] * cell.y[j];
       }
 
-      cells[i].center.m = total_mass;
+      cell.center.m = total_mass;
 
       if (total_mass > 0) {
-        cells[i].center.x = weighted_x / total_mass;
-        cells[i].center.y = weighted_y / total_mass;
+        cell.center.x = weighted_x / total_mass;
+        cell.center.y = weighted_y / total_mass;
       } else {
-        cells[i].center.x = -2 * side;
-        cells[i].center.y = -2 * side;
+        cell.center.x = -2 * side;
+        cell.center.y = -2 * side;
       }
     }
   }
@@ -109,6 +108,7 @@ void compute_centers_of_mass(double side, long ncside,
       cells[ind].center.m = cells[ind2].center.m;
     }
   }
+  // debug_center(ncside, cells);
 }
 
 void compute_kinetics(double side, long ncside, std::vector<cell_t> &cells) {
@@ -116,29 +116,33 @@ void compute_kinetics(double side, long ncside, std::vector<cell_t> &cells) {
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = INDEX(ix, iy, ncside);
-      long long cell_size = cells[i].par.size();
+      cell_t &cell = cells[i];
+      long long cell_size = cell.x.size();
       for (long long j = 0; j < cell_size; j++) {
         for (long long k = j + 1; k < cell_size; k++) {
-          vec_t force =
-              calc_gravitational_force(cells[i].par[j], cells[i].par[k]);
-          cells[i].par[j].ax += force.x;
-          cells[i].par[j].ay += force.y;
-          cells[i].par[k].ax -= force.x;
-          cells[i].par[k].ay -= force.y;
+          vec_t force = calc_gravitational_force(cell, j, k);
+          if(cell.ind[j] == 0) {
+            DEBUG("F %lld and 0: x: %.6f y: %.6lf\n", cell.ind[k], force.x, force.y);
+          }
+          cell.ax[j] += force.x;
+          cell.ay[j] += force.y;
+          cell.ax[k] -= force.x;
+          cell.ay[k] -= force.y;
         }
-
         for (long long k = 0; k < 9; k++) {
           if (k == 4)
             continue;
           long ind = i + ((k % 3) - 1) + (k / 3 - 1) * (ncside + 2);
-          vec_t force =
-              calc_gravitational_force(cells[i].par[j], cells[ind].center);
-          cells[i].par[j].ax += force.x;
-          cells[i].par[j].ay += force.y;
+          vec_t force = calc_gravitational_force(cell, j, cells[ind].center);
+          if(cell.ind[j] == 0) {
+            DEBUG("F center %ld and 0: x: %.6f y: %.6lf\n", ind, force.x, force.y);
+          }
+          cell.ax[j] += force.x;
+          cell.ay[j] += force.y;
         }
-        cells[i].par[j].ax /= cells[i].par[j].m;
-        cells[i].par[j].ay /= cells[i].par[j].m;
-        update_position_and_velocity(side, cells[i].par[j]);
+        cell.ax[j] /= cell.m[j];
+        cell.ay[j] /= cell.m[j];
+        update_position_and_velocity(side, cell, j);
       }
     }
   }
@@ -148,14 +152,13 @@ void debug_particles(long ncside, std::vector<cell_t> &cells) {
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = INDEX(ix, iy, ncside);
-      long long cell_size = cells[i].par.size();
+      long long cell_size = cells[i].x.size();
       for (long long j = 0; j < cell_size; j++) {
         DEBUG("Particle %lld: m: %.6f, x: %.6f, y: %.6f, vx: %.6f, vy: %.6f, "
               "ax: %.6f, ay: %.6f, collided: %d, cell: %ld\n",
-              cells[i].par[j].ind, cells[i].par[j].m, cells[i].par[j].x,
-              cells[i].par[j].y, cells[i].par[j].vx, cells[i].par[j].vy,
-              cells[i].par[j].ax, cells[i].par[j].ay, cells[i].par[j].collided,
-              iy * ncside + ix);
+              cells[i].ind[j], cells[i].m[j], cells[i].x[j], cells[i].y[j],
+              cells[i].vx[j], cells[i].vy[j], cells[i].ax[j], cells[i].ay[j],
+              (int)cells[i].collided[j], iy * ncside + ix);
       }
     }
   }
@@ -167,81 +170,77 @@ void compute_new_particle_cell(double size, long ncside,
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = INDEX(ix, iy, ncside);
-      omp_set_lock(&cells[i].lock);
-      long long cell_size = cells[i].par.size();
-      omp_unset_lock(&cells[i].lock);
+      cell_t &cell = cells[i];
+      omp_set_lock(&cell.lock);
+      long long cell_size = cell.x.size();
+      omp_unset_lock(&cell.lock);
       for (long long j = 0; j < cell_size; j++) {
-        long ind = find_particle_cell(cells[i].par[j], size, ncside);
+        long ind = find_particle_cell(cell, j, size, ncside);
         if (ind == i)
           continue;
         omp_set_lock(&cells[ind].lock);
-        cells[ind].par.push_back(cells[i].par[j]);
+        cells[ind].push_back(cell, j);
         omp_unset_lock(&cells[ind].lock);
-        cells[i].par[j].m = 0;
+        cell.m[j] = 0;
       }
     }
   }
-  // #pragma omp single
-  // debug_particles(ncside, cells);
 }
 
 void detect_collisions(long ncside, std::vector<cell_t> &cells,
                        long long &n_collisions) {
-#pragma omp for collapse(2) reduction(+ : n_collisions)                        \
-    schedule(dynamic, CHUNK_SIZE)
+#pragma omp for collapse(2) reduction(+ : n_collisions) schedule(dynamic, CHUNK_SIZE)                    
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = INDEX(ix, iy, ncside);
       long long cell_collisions = 0;
-      long long cell_size = cells[i].par.size();
+      cell_t &cell = cells[i];
+      long long cell_size = cell.x.size();
       for (long long j = cell_size - 1; j >= 0; j--) {
-        if (cells[i].par[j].m == 0) {
+        if (cell.m[j] == 0) {
           DEBUG("Removing mass null Particle %lld from cells[%ld].par[%lld]\n",
-                cells[i].par[j].ind, i, j);
+                cell.ind[j], i, j);
           cell_size--;
-          copy_particle(cells[i].par[j], cells[i].par[cell_size]);
+          copy_particle(cell, j, cell_size);
         }
       }
       for (long long j = cell_size - 1; j >= 0; j--) {
         for (long long k = j - 1; k >= 0; k--) {
-          double squared_distance =
-              calc_squared_distance(cells[i].par[j], cells[i].par[k]);
+          double squared_distance = calc_squared_distance(cell, j, k);
           if (squared_distance > EPSILON2)
             continue;
           DEBUG("Distance: %.6lf, ix: %ld, iy: %ld, j: %lld, k: %lld\n",
-                squared_distance, ix, iy, cells[i].par[j].ind,
-                cells[i].par[k].ind);
-          if (cells[i].par[k].collided == false) {
+                squared_distance, ix, iy, cell.ind[j], cell.ind[k]);
+          if (cell.collided[k] == false) {
             cell_collisions++;
-            cells[i].par[k].collided = true;
+            cell.collided[k] = true;
           }
-          cells[i].par[j].collided = true;
+          cell.collided[j] = true;
         }
-        if (cells[i].par[j].collided == true) {
+        if (cell.collided[j] == true) {
           DEBUG("Removing Particle %lld from cells[%ld].par[%lld]\n",
-                cells[i].par[j].ind, i, j);
+                cells[i].ind[j], i, j);
           cell_size--;
-          copy_particle(cells[i].par[j], cells[i].par[cell_size]);
+          copy_particle(cell, j, cell_size);
         }
       }
       n_collisions += cell_collisions;
-      cells[i].par.resize(cell_size);
+      cell.resize(cell_size);
     }
   }
-  // #pragma omp single
   // debug_particles(ncside, cells);
 }
 
-void find_particle_zero(long ncside, std::vector<cell_t> &cells,
-                        particle_t &par0) {
-#pragma omp for collapse(2) schedule(dynamic, CHUNK_SIZE)
+void find_particle_zero(long ncside, std::vector<cell_t> &cells, particle_t &par0) {
+  #pragma omp for collapse(2) schedule(dynamic, CHUNK_SIZE)
   for (long iy = 0; iy < ncside; iy++) {
     for (long ix = 0; ix < ncside; ix++) {
       long i = INDEX(ix, iy, ncside);
-      long long cell_size = cells[i].par.size();
+      long long cell_size = cells[i].x.size();
       for (long long j = 0; j < cell_size; j++) {
-        if (cells[i].par[j].ind == 0)
-          copy_particle(par0, cells[i].par[j]);
+        if (cells[i].ind[j] == 0) {
+          copy_particle(par0, cells[i], j);
+        }
       }
     }
   }
@@ -254,7 +253,7 @@ simulation_result simulation(double side, long ncside, long long npart,
   simulation_result res;
   res.number_of_collisions = 0;
   fill_cells(size, ncside, npart, par, cells);
-#pragma omp parallel
+  #pragma omp parallel
   {
     init_cells_lock(ncside, cells);
     for (long long i = 0; i < nstep; i++) {
@@ -264,6 +263,8 @@ simulation_result simulation(double side, long ncside, long long npart,
       compute_kinetics(side, ncside, cells);
       compute_new_particle_cell(size, ncside, cells);
       detect_collisions(ncside, cells, res.number_of_collisions);
+      #pragma omp single
+      debug_particles(ncside, cells);   
     }
     destroy_cells_lock(ncside, cells);
     find_particle_zero(ncside, cells, res.particle_zero);
