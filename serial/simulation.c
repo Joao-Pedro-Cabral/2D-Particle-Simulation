4ce455234bbe37107847a4044e71748ee95ea16c
Joao-Pedro-Cabral <miranda.jp@usp.br>
2025-03-10 17:52:51 +0000

    Adding AVX version of dettect collision

--- serial/simulation.c
+++ serial/simulation.c
@@ -230,6 +230,26 @@ void compute_new_particle_cell(double size, long ncside, long ncside2,
   }
 }
 
+void print_m256d(__m256d vec) {
+    // 4x long long (64-bit)
+    double ll_values[4];
+    _mm256_storeu_pd(ll_values, vec);
+    for (int i = 0; i < 4; i++) {
+        printf("%.9lf ", ll_values[i]);
+    }
+    printf("\n");
+}
+
+void print_m256i(__m256i vec) {
+    // 4x long long (64-bit)
+    long long ll_values[4];
+    _mm256_storeu_si256((__m256i*)ll_values, vec);
+    for (int i = 0; i < 4; i++) {
+        printf("%lld ", ll_values[i]);
+    }
+    printf("\n");
+}
+
 void detect_collisions(long ncside2, cell_t *cells) {
 
   for (long i = 0; i < ncside2; i++) {
@@ -244,22 +264,48 @@ void detect_collisions(long ncside2, cell_t *cells) {
       }
     }
     for (long long j = cell_size - 1; j >= 0; j--) {
-      long long collided = cells[i].collided[j];
-      for (long long k = j - 1; k >= 0; k--) {
+      long long collision = 0;
+      __m256i collidedj = _mm256_set1_epi64x(cells[i].collided[j]);
+      __m256d epsilon2 = _mm256_set1_pd(EPSILON2);
+      __m256d xj =  _mm256_set1_pd(cells[i].x[j]);
+      __m256d yj =  _mm256_set1_pd(cells[i].y[j]);
+      long long k;
+      long long iter = (j) - (j & 3);
+      for (k = 0; k < iter; k+= 4) {
+        __m256d xk =  _mm256_loadu_pd(&cells[i].x[k]);
+        __m256d yk =  _mm256_loadu_pd(&cells[i].y[k]);
+        __m256d dx = _mm256_sub_pd(xk, xj);
+        __m256d dy = _mm256_sub_pd(yk, yj);
+        __m256d distance = _mm256_mul_pd(dx, dx);
+        distance = _mm256_fmadd_pd(dy, dy, distance);
+        __m256d cmp = _mm256_cmp_pd(distance, epsilon2, _CMP_LT_OQ);
+        __m256i near = _mm256_castpd_si256(cmp);
+        __m256i collidedk = _mm256_loadu_si256((__m256i*)&cells[i].collided[k]);
+        _mm256_storeu_si256((__m256i*)&cells[i].collided[k], near);
+        near = _mm256_andnot_si256(collidedj, near);
+        near = _mm256_andnot_si256(collidedk, near);
+        collision += (_mm256_movemask_epi8(near) != 0);
+      }
+      while(k < j) {
         double dx = cells[i].x[j] - cells[i].x[k];
         double dy = cells[i].y[j] - cells[i].y[k];
         double distance = dx * dx + dy * dy;
-        if (distance > EPSILON2)
+        if (distance > EPSILON2) {
+          k++;
           continue;
+        }
         DEBUG("Distance: %.6lf, i: %ld, j: %lld, k: %lld\n", distance, i,
               cells[i].ind[j], cells[i].ind[k]);
-        if (cells[i].collided[k] == 0 && (collided == 0)) {
-          cell_collisions++; 
+        if (cells[i].collided[k] == 0) {
+          collision++; 
         }
         cells[i].collided[k] = -1;
-        collided = -1;
+        k++;
       }
-      cells[i].collided[j] = collided;
+      if(collision > 0 && (cells[i].collided[j] == 0)) {
+        cells[i].collided[j] = -1;
+        cell_collisions ++;
+      }
       if (cells[i].collided[j] == -1) {
         DEBUG("Removing Particle %lld from cells[%ld].par[%lld]\n",
               cells[i].ind[j], i, j);
